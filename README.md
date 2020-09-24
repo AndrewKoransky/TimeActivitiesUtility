6b5613df728b9a659c54eb88a51a45a0a23c4dd2
Andrew Koransky <andrew_koransky@aar.com>
2020-09-24 10:24:49 -0600

    add clean script, readme, screenshot, vs2019 sln

---
+++ README.md
@@ -0,0 +1,4 @@
+# TimeActivitiesUtility
+Time Activities Utility - records elaped time for multiple activites. Persists data to My Documents\TimeActivitiesUtility.csv. Screenshot:
+
+![Time Activities Screenshot](screenshot.png)
---
+++ clean.bat
@@ -0,0 +1,6 @@
+if exist .vs rmdir /s /q .vs
+if exist packages rmdir /s /q packages
+if exist TimeActivitiesUtility.v3.ncrunchsolution.user del TimeActivitiesUtility.v3.ncrunchsolution.user
+if exist TimeActivitiesUtility\bin rmdir /s /q TimeActivitiesUtility\bin
+if exist TimeActivitiesUtility\obj rmdir /s /q TimeActivitiesUtility\obj
+if exist TimeActivitiesUtility.v3.ncrunchsolution.user del TimeActivitiesUtility.v3.ncrunchsolution.user
