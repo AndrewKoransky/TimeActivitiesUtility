bfd5641f92261b0a42a56d6c33a0bae953c52b47
Andrew Koransky <andrew@koransky.com>
2020-11-06 14:30:46 -0700

    save window size in user profile a better solution would be: https://stackoverflow.com/a/847828/530176

--- TimeActivitiesUtility/MainWindow.xaml.cs
+++ TimeActivitiesUtility/MainWindow.xaml.cs
@@ -26,5 +26,41 @@ namespace TimeActivitiesUtility
             InitializeComponent();
             DataContext = ViewModel.MainWindowVM.Create(new Service.UpdateTimerDialogService());
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                // Use the RestoreBounds as the current values will be 0, 0 and the size of the screen
+                Properties.Settings.Default.Top = RestoreBounds.Top;
+                Properties.Settings.Default.Left = RestoreBounds.Left;
+                Properties.Settings.Default.Height = RestoreBounds.Height;
+                Properties.Settings.Default.Width = RestoreBounds.Width;
+                Properties.Settings.Default.Maximized = true;
+            }
+            else
+            {
+                Properties.Settings.Default.Top = this.Top;
+                Properties.Settings.Default.Left = this.Left;
+                Properties.Settings.Default.Height = this.Height;
+                Properties.Settings.Default.Width = this.Width;
+                Properties.Settings.Default.Maximized = false;
+            }
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            this.Top = Properties.Settings.Default.Top;
+            this.Left = Properties.Settings.Default.Left;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+            // Very quick and dirty - but it does the job
+            if (Properties.Settings.Default.Maximized)
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
     }
 }
